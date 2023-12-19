fd5cadfc92d7538c135b1f0b6ad97637b755a1b4
tech7895 <techhunter990@gmail.com>
2023-12-19 00:29:47 -0800

    updated

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -63,8 +63,8 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 }
 
 /**
- * cocktail_sort_list - This sorts a listint_t doubly-linked list of integers in
- *                      ascending order using the cocktail shaker algorithm.
+ * cocktail_sort_list - This sorts a listint_t doubly-linked list of integers
+ * in ascending order using the cocktail shaker algorithm.
  * @list: A pointer to the head of
  * a listint_t doubly-linked list
  *
